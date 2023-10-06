5d7f3875b5acb44fcc0eb69904c9930c5ffd9ee0
Hannah Chamberlin <hannahchloechamberlin@gmail.com>
2023-10-06 15:53:49 +0100

    Adding comments to step 3

--- s3_nii_to_meshes.py
+++ s3_nii_to_meshes.py
@@ -8,6 +8,24 @@ from scipy import ndimage
 # FUNCTION DEFINITIONS 
 
 def load_n_mesh(fname):
+    """
+    load_n_mesh : Converts individual nifti files to .ply meshes 
+
+
+    Parameters
+    ----------
+    fname : filepath
+        The filepath to the nifti file to be converted to the mesh
+    
+    Returns
+    -------
+    mesh : o3d.geometry.TriangleMesh()
+        the triangulated mesh created from the nifti file  
+    
+    mask : np.array 
+        np.array created from the conversion of the nifti file using SimpleITK
+
+    """
     # load data
     nii = sitk.ReadImage(fname)
     direction_matrix = nii.GetDirection();# --> covariance matic, orientation of the image, same for all masks
@@ -32,16 +50,74 @@ def load_n_mesh(fname):
 
 # smoothing taubin function
 def smooth_n_simplify(mesh):
+    """
+    Optional function to apply smoothing to the mesh returned from load_n_mesh. Taubin smoothing included as an example
+
+
+    Parameters
+    ----------
+    mesh : o3d.geometry.TriangleMesh()
+        the triangulated mesh created from the nifti file 
+    
+    Returns
+    -------
+    mesh : o3d.geometry.TriangleMesh()
+        the triangulated mesh created from the nifti file, which has been smoothed by this function
+    
+    """
+    
     #mesh = mesh.filter_smooth_taubin(number_of_iterations=1)
+
     return mesh
 
 class NiftiFile:
+    """
+    A class to store a filepath to a nifti file, and store the name for ease of use in the remaining code.
+
+    ...
+
+    Attributes
+    ----------
+    filepath : str
+        filepath to the nifti file
+    name : str
+        name of the nifti file, i.e. the filepath without the extension
+
+    Methods
+    -------
+    None
+    """
     def __init__(self, filepath, name):
         self.filepath = filepath;
         self.name = name;
 
 def s3_main(nifti_base_dir, mesh_base_dir, patient_IDs, observers, sides, contour_names, organ_name = "breast"):
+    """
+    Step 3 main function: Convert nifit files to .ply meshes by looping over patient number, organ laterality, and contour names. 
 
+
+    Parameters
+    ----------
+    nifti_base_dir : filepath
+        The filepath which is the base directory for all the patient directories which store patients' nifti files (created in step 1).
+    mesh_base_dir : filepath
+        The filepath which is the base directory for all the patient directories which store patients' mesh files, created in this step.    
+    patient_IDs : array of str
+        The patient numbers, which are also the directory names for the directories in nifti_base_dir
+    observers : array of str
+        Array of strings which idicate the observer which generated the organ contour. Included in the naming of the region of interest contour in you treatment planning system, thus included in the naming of the nifti file sed to generate the STAPLE contour.      
+    sides: array of str
+        The laterality of the organ contour (here used to loop over left and right breast nifti files). Used to select the contours to be included in the STAPLE algorithm, i.e. only uses left contours to create the left breast STAPLE contour.
+    contour_names : array of str
+        The type of the organ contour. Example used here is "manual" contours and "altas-edited" contours, as we are do inter observer and inter-method analysis simultaneously. Used to select the contours to be included in the STAPLE algorithm, i.e. only uses "manual" contours to create the "manual breast STAPLE contour").
+    organ_name : str
+        The organ name which is featured in the name of the region of interest's nifti file.
+    
+ 
+    Returns
+    -------
+    None
+    """
     for patient in patient_IDs: 
         print("           Working with patient " + str(patient))
         #set current directory (patient folder with nifti masks)
@@ -56,30 +132,28 @@ def s3_main(nifti_base_dir, mesh_base_dir, patient_IDs, observers, sides, contou
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
 
-        #######################################################################
-        # # BEGINNING OF READ AND SELECT NIFTIs
-        #######################################################################
-        nifty_files = [];
-
-        ### read nifti files 
+        ### read nifti files and store to array 
+        nifti_files = [];
+        # loop over organ laterality
         for side in sides:
+            # loop over contour names included in file name (here either "manual" or "atlas-edited")
             for contour in contour_names:
-                #STAPLE contour
-                nifty_files.append(NiftiFile(f"{side}_{organ_name}_{contour}_staple.nii" , f"{side}_{organ_name}_{contour}_staple"))
+                # STAPLE contour
+                nifti_files.append(NiftiFile(f"{side}_{organ_name}_{contour}_staple.nii" , f"{side}_{organ_name}_{contour}_staple"))
             
                 # comparison contours 
                 for n in range(0,len(observers)):
-                    nifty_files.append(NiftiFile(f"{side}{observers[n]}{contour}.nii" , f"{side}_{contour}_{n+1}"))
+                    nifti_files.append(NiftiFile(f"{side}{observers[n]}{contour}.nii" , f"{side}_{contour}_{n+1}"))
 
-        print("read nifti files")
-        # # END OF READ AND SELECT NIFTIs
+        print("finished reading nifti files")
         
-        for example in nifty_files:
+        #loop over nifti files and convert to meshes
+        for example in nifti_files:
             # convert to meshes and masks
             mesh, mask = load_n_mesh(example.filepath) 
             # smooth meshes
             mesh = smooth_n_simplify(mesh)
-            #save to file
+            #save to .ply file
             o3d.io.write_triangle_mesh(os.path.join(output_dir, f"{example.name}.ply"), mesh)
             print("Converted " + example.name)
 
